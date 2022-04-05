b67c75d0b4d9f57672668aa0805e74f970d7342c
gustavosimon <gustavo.andre.simon@hotmail.com>
2022-04-05 20:36:26 -0300

    first commit

---
+++ src/segmentation/challenge/SegmentationChallenge.java
@@ -0,0 +1,16 @@
+package segmentation.challenge;
+
+/**
+ *
+ * @author Gustavo Simon
+ */
+public class SegmentationChallenge {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
