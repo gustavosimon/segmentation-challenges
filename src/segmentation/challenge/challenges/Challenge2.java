261dfc740bc4409283087957e75563b8f5fad01d
gustavosimon <gustavo.andre.simon@hotmail.com>
2022-04-10 11:53:48 -0300

    Solved the second challenge

--- src/segmentation/challenge/challenges/Challenge2.java
+++ src/segmentation/challenge/challenges/Challenge2.java
@@ -1,8 +1,15 @@
 package segmentation.challenge.challenges;
 
-import segmentation.challenge.window.Processed;
+import segmentation.challenge.bean.Image;
+import segmentation.challenge.utils.ImageUtils;
 
 import static segmentation.challenge.utils.ImageConstants.*;
+import static java.awt.image.BufferedImage.TYPE_INT_RGB;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+
+import java.util.Optional;
 
 /**
  * Classe para resolver o desafio 2.
@@ -14,13 +21,50 @@ import static segmentation.challenge.utils.ImageConstants.*;
  */
 public final class Challenge2 implements Challenge {
 
+    /** Máscara para aplicar filtro Gaussiano */
+    private static final int[][] mask = {{ 1,  2,  1 },
+                                         { 2,  4,  2 },
+                                         { 1,  2,  1 }};
+
     /** Construtor do desafio 2 */
     public Challenge2() {}
 
     @Override
     public void solveChallenge() {
-        Processed p = new Processed(CHALLENGE_2_PATH);
-        p.setVisible(true);
+        Optional<Image> optImage = ImageUtils.getImageFromFile(CHALLENGE_2_PATH);
+        if (optImage.isEmpty()) {
+            return;
+        }
+        Image image = optImage.get();
+        int[][] matrix = image.getMatrix();
+        BufferedImage processed = new BufferedImage(image.getWidth(), image.getHeight(), TYPE_INT_RGB);
+        double redSum = 0;
+        double greenSum = 0;
+        double blueSum = 0;
+        int newRed = 0;
+        int newGreen = 0;
+        int newBlue = 0;
+        for (int y = 1; y < image.getHeight() - 1; y++) {
+            for (int x = 1; x < image.getWidth() - 1; x++) {
+                redSum = 0;
+                greenSum = 0;
+                blueSum =  0;
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        Color color = new Color(matrix[x + (i - 1)][y + (j - 1)]);
+                        redSum += color.getRed() * mask[i][j];
+                        greenSum += color.getGreen() * mask[i][j];
+                        blueSum += color.getBlue() * mask[i][j];
+                    }
+                }
+                newRed = (int) redSum / 16;
+                newGreen = (int) greenSum / 16;
+                newBlue = (int) blueSum / 16;
+                processed.setRGB(x, y, new Color(newRed, newGreen, newBlue).getRGB());
+            }
+        }
+        ImageUtils.writeImage(processed, PROCESSED_IMAGE_PATH);
+        ImageUtils.showProcessedImage();
     }
 
 }
